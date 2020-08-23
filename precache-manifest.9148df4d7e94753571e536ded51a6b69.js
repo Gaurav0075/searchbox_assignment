c4e3ea345a71c30ac5fc8ffd502716ed2edbafca
Gaurav <gaurav.kumar0075@gmail.com>
2020-08-24 04:46:09 +0530

    Updates

---
+++ precache-manifest.9148df4d7e94753571e536ded51a6b69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "905650bf15150c02984df0cbf95fc567",
+    "url": "/searchbox_assignment/index.html"
+  },
+  {
+    "revision": "006156fbad85b86e3fdc",
+    "url": "/searchbox_assignment/static/css/main.7259c817.chunk.css"
+  },
+  {
+    "revision": "7feb5cf05d296705ac6a",
+    "url": "/searchbox_assignment/static/js/2.8db86a1f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/searchbox_assignment/static/js/2.8db86a1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "006156fbad85b86e3fdc",
+    "url": "/searchbox_assignment/static/js/main.08296a86.chunk.js"
+  },
+  {
+    "revision": "09bcfc890e1abd0fb617",
+    "url": "/searchbox_assignment/static/js/runtime-main.d0de5380.js"
+  }
+]);
